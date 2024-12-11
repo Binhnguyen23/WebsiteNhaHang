d9a0fd4ac93dbecf039336a54196fae8d680953a
Nguyễn Thanh Bình <149800466+Binhnguyen23@users.noreply.github.com>
2024-12-11 10:18:37 +0700

    Add files via upload

---
+++ flask-food-order-system-master/wsgi.py
@@ -0,0 +1,7 @@
+from app import app # (app là đối tượng "Flask" từ trong file "app.py")
+
+if __name__ == '__main__': # lệnh thực thi cho phương thức "run()" của đối tượng "app"
+    app.run(debug=True) # gọi phương thức "run()" từ đối tượng "app" # --> 
+
+
+                                              
